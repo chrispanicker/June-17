8941f8a7c3e1cb39c005a1857e3934ff55259220
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-06-16 15:18:07 -0400

    test

--- main.js
+++ main.js
@@ -13,15 +13,6 @@ Vertices=Matter.Vertices;
 var engine = Engine.create(),
 world = engine.world;
 
-// let gyroscope = new Gyroscope({ frequency: 60 });
-
-// gyroscope.addEventListener("reading", (e) => {
-//     console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-//     console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-//     console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-// });
-// gyroscope.start();
-
 engine.gravity.x=0
 engine.gravity.y=1
 
