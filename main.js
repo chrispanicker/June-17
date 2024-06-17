6d57843e8af2a4993bd0696efb1cd4265fb1fb8e
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-06-17 03:30:19 -0400

    done

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ var vy = 0.0;
 var updateRate = 1/60; 
 var index = 0;
 let rotation, leftToRight = 0;
-let frontToBack = 0;
+let frontToBack = 1;
 let xGravity = 0;
 let yGravity = 1;
+
+let color2 = "rgb(37, 63, 36)";
+let color = "rgb(195, 255, 0)"
 
 function map (n, start1, stop1, start2, stop2, withinBounds) {
   const newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
@@ -51,6 +54,7 @@ var Engine = Matter.Engine,
 Render = Matter.Render,
 Runner = Matter.Runner,
 Composites = Matter.Composites,
+Constraint = Matter.Constraint,
 Common = Matter.Common,
 MouseConstraint = Matter.MouseConstraint,
 Mouse = Matter.Mouse,
@@ -88,6 +92,8 @@ var render = Render.create({
 
 Render.run(render);
 
+
+
 var runner = Runner.create();
 Runner.run(runner, engine);
 
@@ -97,14 +103,14 @@ let boxA, boxB, letterOffset;
 if(canvas.clientWidth<720){
     boxA=Bodies.rectangle(canvas.clientWidth/2, canvas.clientHeight-60, 240, 50,{
         render:{
-            fillStyle: "rgb(255, 247, 0)"
+            fillStyle: color2
         },
         isStatic: true
     })
 
     boxB=Bodies.rectangle(canvas.clientWidth/2, 0+20, 240, 50,{
         render:{
-            fillStyle: "rgb(255, 247, 0)"
+            fillStyle:color2
         },
         isStatic: true
     })
@@ -113,14 +119,14 @@ if(canvas.clientWidth<720){
  }else{
     boxA=Bodies.rectangle(canvas.clientWidth/2, canvas.clientHeight-80, 365, 90,{
         render:{
-            fillStyle: "rgb(255, 247, 0)"
+            fillStyle: color2
         },
         isStatic: true
     })
 
     boxB=Bodies.rectangle(canvas.clientWidth/2, 0+50, 260, 50,{
         render:{
-            fillStyle: "rgb(255, 247, 0)"
+            fillStyle: color2
         },
         isStatic: true
     })
@@ -128,66 +134,98 @@ if(canvas.clientWidth<720){
  }
 
 let letters = [document.querySelector("#S1"), document.querySelector("#E1"), document.querySelector("#L"), document.querySelector("#E2"), document.querySelector("#C"), document.querySelector("#T"), document.querySelector("#S2")];
-
 letters.forEach((e, i)=>{
     let scaleFactor
      let vertices = Svg.pathToVertices(e);
      if(canvas.clientWidth<1080){
         scaleFactor = (800* .2)/100;
      }else{
-        scaleFactor = (800* .4)/100;
+        scaleFactor = (800* .3)/100;
      }
 
      vertices = Vertices.scale(vertices,scaleFactor,scaleFactor);
      let svgBody = Bodies.fromVertices(
-        canvas.clientWidth/letters.length + canvas.clientWidth/letters.length*i +letterOffset,
-        200,
+        canvas.clientWidth/letters.length + canvas.clientWidth/letters.length*i +letterOffset, canvas.clientHeight/3,
         [vertices],
         {
             render:{
-                fillStyle: `rgb(${i* 255/letters.length}, ${getRandomInt(0,255)}, 255)`
+                fillStyle: `rgb(${(i+1)* 255/letters.length}, 255, 0)`
             }
         }
      )
 
      letters[i] = svgBody
-
-     Matter.Body.setMass(letters[i], 10)
-     console.log(letters[i])
 })
 
-
+var constraints = [];
+letters.forEach((e, i)=>{
+    if(i<letters.length-1){
+        constraints[i] = Constraint.create({
+            bodyA: letters[i],
+            pointA: { x: -10, y: -10 },
+            bodyB: letters[i+1],
+            pointB: { x: -10, y: -10 },
+            render:{
+                anchors:false,
+            }
+        });
+    }  
+})
 
 let thickness = 50;
 let boundTop = Bodies.rectangle(canvas.clientWidth/2, 0-thickness/2, canvas.clientWidth, thickness, { isStatic: true, render:{
-    fillStyle: "rgb(255, 247, 0)"
+    fillStyle: color2
 }, })
-let boundBot = Bodies.rectangle(canvas.clientWidth/2, canvas.clientHeight+thickness/2-thickness, canvas.clientWidth, thickness, { isStatic: true, render:{
-    fillStyle: "rgb(255, 247, 0)"
+let boundBot = Bodies.rectangle(canvas.clientWidth/2, canvas.clientHeight+thickness/2, canvas.clientWidth, thickness, { isStatic: true, render:{
+    fillStyle: color2
 }, })
 let boundRight = Bodies.rectangle(canvas.clientWidth+thickness/2, canvas.clientHeight/2, thickness, canvas.clientHeight, { isStatic: true, render:{
-    fillStyle: "rgb(255, 247, 0)"
+    fillStyle: color2
 }, })
 let boundLeft = Bodies.rectangle(0-thickness/2, canvas.clientHeight/2, thickness, canvas.clientHeight, { isStatic: true, render:{
-    fillStyle: "rgb(255, 247, 0)"
+    fillStyle: color2
 }, })
+
+var startPin = Constraint.create({
+    pointA: { x: 150, y: canvas.clientHeight/4 },
+    bodyB: letters[0],
+    pointB: { x: -10, y: -10 },
+    damping: 1,
+});
+
+var endPin = Constraint.create({
+    pointA: { x: canvas.clientWidth-150, y: canvas.clientHeight/4 },
+    bodyB: letters[6],
+    pointB: { x: -10, y: -10 },
+    damping: 1,
+});
+
+
+// Composite.add(world, [bodyA, bodyB, constraint]);
 
 Composite.add(world, [
     // walls
     boundTop,
     boundBot,
     boundLeft,
-    boundRight,
-    boxA,
-    boxB,
-
+    boundRight,    
+    startPin,
+    constraints[0],
+    constraints[1],
+    constraints[2],
+    constraints[3],
+    constraints[4],
+    constraints[5],
+    endPin,
     letters[0],
     letters[1],
     letters[2],
     letters[3],
     letters[4],
     letters[5],
-    letters[6]
+    letters[6],
+
+    // constraints[6],
 ]);
 
 
